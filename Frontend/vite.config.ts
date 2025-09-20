446d12646f5065d4739128d7eea3526eee91ee35
matthew-ngzc <82407622+matthew-ngzc@users.noreply.github.com>
2025-09-21 07:11:50 +0800

    changed define config to exporting plain object instead of function

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,31 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
+const BACKEND = process.env.VITE_BACKEND_URL || 'http://localhost:3000'
 
-  return {
-    plugins: [
-      vue(),
-      vueJsx(),
-      // vueDevTools(),
-      nightwatchPlugin(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    // vueDevTools(),
+    nightwatchPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    server: {
-      proxy: {
-        '/api': env.VITE_BACKEND_URL
-      }
+  },
+  server: {
+    proxy: {
+      '/api': BACKEND
     }
   }
 })
